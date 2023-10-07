ec321080f3e5f900df44a87370f0e7d3a734b59a
Consuelo Sierra <asierralopez526@gmail.com>
2023-10-06 20:52:53 -0600

    Add basic TopNav styling on unsaved file

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const TopNav = () => {
     return (
-        <div className='max-w-1520 mx-auto flex justify-between'>
-        
+        <div className='max-w-[1520] mx-auto flex justify-between items-center p-4'>
+            <div className='flex items-center'>
+                <div className='cursor-pointer'>
+
+                </div>
+            </div>
         </div>
     )
 }
