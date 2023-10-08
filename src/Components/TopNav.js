269519a446ba6157489fe3e9bbdfc71642c78303
Consuelo Sierra <asierralopez526@gmail.com>
2023-10-07 18:28:09 -0600

    Add ginal icons to the nav a funtionality

--- src/Components/TopNav.js
+++ src/Components/TopNav.js
@@ -3,9 +3,11 @@ import {
      AiOutlineMenu, 
      AiOutlineSearch,
      AiOutlineClose,
-     AiFillTag
 } from 'react-icons/ai'
 import {BsFillCartFill, BsPerson} from 'react-icons/bs'
+import {TbTruckReturn} from 'react-icons/tb'
+import {FaGoogleWallet} from 'react-icons/fa'
+import {MdHelp, MdOutlineFavorite} from 'react-icons/md'
 
 const TopNav = () => {
     const [sideNav, setSideNav] = useState(false)
@@ -43,7 +45,53 @@ const TopNav = () => {
                 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-200' 
                 : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-200'}
             >
-
+                <AiOutlineClose
+                  onClick={() => setSideNav(!sideNav)}
+                  size={25}
+                  className='absolute right-4 top-4 cursor-pointer'
+                />
+                <h2 className='text-2xl p-4'>YUM
+                    <span className='text-orange-700 font-bold'>YUM</span>
+                </h2>
+                <nav>
+                    <ul className='flex flex-col p-4 text-gray-900'>
+                        <li className='text-xl py-4 flex'>
+                            <BsPerson 
+                                size={25}
+                                className='mr-4 text-white bg-black rounded-full'
+                            />
+                            My Account
+                        </li>
+                        <li className='text-xl py-4 flex'>
+                            <TbTruckReturn 
+                                size={25}
+                                className='mr-4 text-white bg-black rounded-full'
+                            />
+                            Delivery
+                        </li>
+                        <li className='text-xl py-4 flex'>
+                            <MdOutlineFavorite
+                                size={25}
+                                className='mr-4 text-white bg-black rounded-full'
+                            />
+                            My Favourites
+                        </li>
+                        <li className='text-xl py-4 flex'>
+                            <FaGoogleWallet
+                                size={25}
+                                className='mr-4 text-white bg-black rounded-full'
+                            />
+                            My Wallet
+                        </li>
+                        <li className='text-xl py-4 flex'>
+                            <MdHelp 
+                                size={25}
+                                className='mr-4 text-white bg-black rounded-full'
+                            />
+                            Help
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     )
