631142154686304046f828b0136c512d8f7981cb
Consuelo Sierra <asierralopez526@gmail.com>
2023-10-13 20:33:33 -0600

    Add functionality to next and previous icons to slide images

--- src/Components/FeaturedItem.js
+++ src/Components/FeaturedItem.js
@@ -15,13 +15,27 @@ const FeaturedItems = () => {
     ]
 
     const [currentIndex, setCurrentIndex] = useState(0)
+
+    const prevSlider = () => {
+        const isFirstSlide = currentIndex === 0
+        const newIndex = isFirstSlide ? sliders.length - 1 : currentIndex - 1
+        setCurrentIndex(newIndex)
+    }
+
+    const nextSlider = () => {
+        const isLastSlide = currentIndex === sliders.length - 1
+        const newIndex = isLastSlide ? 0 : currentIndex + 1
+        setCurrentIndex(newIndex)
+    }
     
     return (
-        <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative'> 
+        <div className='max-w-[1520px] h-[500px] w-full py-4 px-4 relative group'> 
             <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
                 style={{backgroundImage:`url(${sliders[currentIndex].url})`}}
             ></div>
             <div className='
+              hidden
+              group-hover:block
               absolute 
               top-[50%] 
               -translate-x-0 
@@ -34,9 +48,11 @@ const FeaturedItems = () => {
               text-white
               cursor-pointer
             '>
-                <BsChevronBarLeft/>
+                <BsChevronBarLeft onClick={prevSlider}/>
             </div>
             <div className='
+              hidden
+              group-hover:block
               absolute 
               top-[50%] 
               -translate-x-0 
@@ -49,7 +65,7 @@ const FeaturedItems = () => {
               text-white
               cursor-pointer
             '>
-                <BsChevronBarRight/>
+                <BsChevronBarRight onClick={nextSlider}/>
             </div>
         </div>
     )
